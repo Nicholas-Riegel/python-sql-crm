b1d1054ee70d21a302fdc569c31acc7afc70152a
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-06-05 16:48:50 -0400

    mvp final version

--- index.py
+++ index.py
@@ -5,11 +5,11 @@ connection = psycopg2.connect(database="crm_python")
 cursor = connection.cursor()
 
 print("Welcome to our CRM")
-answer = input("What would you like to do? \n1. See all employees. \n2. See all companies.\n3. See one employee. \n4. See one company. \n5. Creat new Employee. \n6. Create new company. \n7. Update an employee.  \n8. Update a company. \n9.Delete an Employee. \n10. Delete a Company. \nPlease enter 1, 2, 3, 4, 5, 6, 7, 8, 9, or 10.\n")
+answer = input("What would you like to do? \n1. See all employees. \n2. See all companies.\n3. See one employee. \n4. See one company. \n5. Create new Employee. \n6. Create new company. \n7. Update an employee.  \n8. Update a company. \n9.Delete an Employee. \n10. Delete a Company. \nPlease enter 1, 2, 3, 4, 5, 6, 7, 8, 9, or 10.\n")
 
 
-# read all employees
 if answer == '1':
+    # read all employees
     cursor.execute("SELECT employees.name AS name, age, companies.name AS company FROM employees JOIN companies ON employees.company_id = companies.id")
     response = cursor.fetchall()
     for employee in response:
@@ -17,25 +17,48 @@ if answer == '1':
         print(f"Name: {name}, Age: {age}, Company: {company}")
         # print(*employee)
 
-# read all companies
 elif answer == '2':
+    # read all companies
     cursor.execute("SELECT name FROM companies")
     response = cursor.fetchall()
-    for company in response:
-        name, age, company = employee
-        print(f"Company: {name}")
-        # print(*company)
+    for companyName in response:
+        print(f"Company: {companyName[0]}")
 
-# create one employee
 elif answer == '3':
+    # read one employee
+    answer2 = input("Please enter employee's name.")
+    cursor.execute("SELECT employees.name AS name, age, companies.name AS company FROM employees LEFT JOIN companies ON employees.company_id = companies.id WHERE employees.name = %s", [answer2])
+    response = cursor.fetchall()
+
+    if response:
+        for employee in response:
+            name, age, company = employee
+            print(f"Name: {name}, Age: {age}, Company: {company}")
+    else:
+        print("Employee not found.")
+
+elif answer == '4':
+    # read one company    
+    answer = input("Please enter company's name.")
+    cursor.execute("SELECT name FROM companies WHERE name = %s", [answer])
+    response = cursor.fetchall()
+    if response:
+        for company in response:
+            name = company[0]
+            print(f"Company: {name}")
+    else:
+        print("Company not found.")
+
+elif answer == '5':
+    # create one employee
     name = input("Please enter employee's name.")
     age = input("Please enter employee's age.")
     company = input("Please enter employee's company.")
    
     cursor.execute("SELECT id FROM companies WHERE name = %s", [company])
+    companyId = cursor.fetchone()
     
-    if cursor.fetchone():
-        companyId = cursor.fetchone()
+    if companyId:
         cursor.execute("INSERT INTO employees (name, age, company_id) VALUES (%s, %s, %s)", [name, age, companyId])
         connection.commit()
     
@@ -50,70 +73,34 @@ elif answer == '3':
 
     print("Employee created.")
 
-# read one employee
-elif answer == '4':
-    
-    answer2 = input("Please enter employee's name.")
-
-    cursor.execute("SELECT employees.name AS name, age, companies.name AS company FROM employees LEFT JOIN companies ON employees.company_id = companies.id WHERE employees.name = %s", [answer2])
-
-    response = cursor.fetchall()
-
-    if response:
-        for employee in response:
-            name, age, company = employee
-            print(f"Name: {name}, Age: {age}, Company: {company}")
-    else:
-        print("Employee not found.")
-
-# read one company
-elif answer == '5':
-    
-    answer = input("Please enter company's name.")
-
-    cursor.execute("SELECT name FROM companies WHERE name = %s", [answer])
-
-    response = cursor.fetchall()
-
-    if response:
-        for company in response:
-            name = company[0]
-            print(f"Company: {name}")
-    else:
-        print("Company not found.")
-# create one company
 elif answer == '6':
+    # create one company
     name = input("Please enter company's name.")
     cursor.execute("INSERT INTO companies (name) VALUES (%s)", [name])
     connection.commit()
     print("Company created.")
-# delete one employee
+
 elif answer == '7':
-    name = input("Please enter employee's name.")
-    cursor.execute("DELETE FROM employees WHERE name = %s", [name])
-    connection.commit()
-    print("Employee deleted.")
-# delete one company
-elif answer == '8':
-    name = input("Please enter company's name.")
-    cursor.execute("DELETE FROM companies WHERE name = %s", [name])
-    connection.commit()
-    print("Company deleted.")
-# update one employee
-elif answer == '9':
+    # update one employee
     name = input("Please enter employee's name.")
     cursor.execute("SELECT * FROM employees WHERE name = %s", [name])
     employee = cursor.fetchone()
     if employee:
         id, name, age, companyId = employee
         
-        cursor.execute("SELECT name FROM companies WHERE id = %s", [companyId])
-        company = cursor.fetchone()[0]
-
-        print(f"Name: {name}, Age: {age}, Company: {company}")
+        company = ''
+        
+        if companyId:
+            cursor.execute("SELECT name FROM companies WHERE id = %s", [companyId])
+            company = cursor.fetchone()[0]
+            print(f"Name: {name}, Age: {age}, Company: {company}")
+        else:
+            print(f"Name: {name}, Age: {age} Company: None")
+        
         newName = input("Please enter new employee's name.")
         newAge = input("Please enter new employee's age.")
         newCompany = input("Please enter new employee's company.")
+        
         if newCompany == company:
             cursor.execute("UPDATE employees SET name = %s, age = %s WHERE id = %s", [newName, newAge, id])
             connection.commit()
@@ -134,8 +121,9 @@ elif answer == '9':
                 print("Employee updated.")
     else:
         print("Employee not found.")
-# update one company
-elif answer == '10':
+
+elif answer == '8':
+    # update one company
     name = input("Please enter company's name.")
     cursor.execute("SELECT * FROM companies WHERE name = %s", [name])
     company = cursor.fetchone()
@@ -148,6 +136,25 @@ elif answer == '10':
         print("Company updated.")
     else:
         print("Company not found.")
+
+elif answer == '9':
+    # delete one employee
+    name = input("Please enter employee's name.")
+    cursor.execute("DELETE FROM employees WHERE name = %s", [name])
+    connection.commit()
+    print("Employee deleted.")
+
+elif answer == '10':
+    # delete one company
+    name = input("Please enter company's name.")
+    cursor.execute("SELECT id FROM companies WHERE name = %s", [name])
+    companyId = cursor.fetchone()
+    if companyId:
+        cursor.execute('UPDATE employees SET company_id = NULL WHERE company_id = %s', [companyId])
+        connection.commit()
+    cursor.execute("DELETE FROM companies WHERE name = %s", [name])
+    connection.commit()
+    print("Company deleted.")
 
 cursor.close()
 connection.close()
